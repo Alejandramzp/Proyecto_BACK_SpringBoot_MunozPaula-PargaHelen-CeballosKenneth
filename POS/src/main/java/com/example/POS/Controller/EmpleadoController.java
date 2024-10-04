a40caae5797ca17af9947d615e19d8d445277411
hnmurillo <hnmurillo24@gmail.com>
2024-10-04 14:59:48 -0500

    feat: :construction: se finalizo la parte de controller para el rol de administrador

---
+++ POS/src/main/java/com/example/POS/Controller/EmpleadoController.java
@@ -0,0 +1,2 @@
+package com.example.POS.Controller;public class EmpleadoController {
+}
---
+++ POS/src/main/java/com/example/POS/Controller/VentaProductoController.java
@@ -0,0 +1,50 @@
+package com.example.POS.Controller;
+
+import com.example.POS.Model.EmpleadoModel;
+import com.example.POS.Model.VentasProductoModel;
+import com.example.POS.Service.EmpleadoService;
+import com.example.POS.Service.VentasProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/admin")
+public class VentaProductoController {
+    @Autowired
+    private VentasProductoService ventasProductoService;
+
+    // obtener todos las ventas por producto
+
+    @GetMapping("/ventaProducto")
+
+    public List<VentasProductoModel> getAllEmpleados(){
+        return ventasProductoService.getAllVentasProducto();
+    }
+
+    @PostMapping("/ventaProducto")
+
+    public VentasProductoModel saveVentaProducto (@RequestBody VentasProductoModel empleado){
+        return ventasProductoService.saveVentasProducto(empleado);
+    }
+
+    @GetMapping ("/ventaProducto/{id}")
+    public Optional<VentasProductoModel> getVentasProductoById(@PathVariable Long id){
+        return ventasProductoService.getVentasProductoById(id);
+    }
+
+    @PutMapping("/ventaProducto/{id}")
+    public VentasProductoModel actualizarVentasProducto (@PathVariable long id, @RequestBody VentasProductoModel ventaProducto) {
+        return ventasProductoService.actualizarVentasProducto(id, ventaProducto);
+    }
+
+    @DeleteMapping("/ventaProducto/{id}")
+    public void eliminareVentasProducto(@PathVariable long id){
+        ventasProductoService.eliminarVentasProducto(id);
+
+
+
+    }
+}
