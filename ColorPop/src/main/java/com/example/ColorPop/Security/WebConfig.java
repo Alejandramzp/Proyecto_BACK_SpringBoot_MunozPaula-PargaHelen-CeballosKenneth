7c6fec17a788813d5685d57530820961fdca985c
Paula <alejandrapmp17@gmail.com>
2024-10-09 15:28:10 -0500

    feat: :lock: Se agregaron los Cors

--- ColorPop/src/main/java/com/example/ColorPop/Security/WebConfig.java
+++ ColorPop/src/main/java/com/example/ColorPop/Security/WebConfig.java
@@ -1,8 +1,12 @@
 package com.example.ColorPop.Security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -12,8 +16,28 @@ public class WebConfig implements WebMvcConfigurer {
         // Configura CORS para los endpoints que comienzan con /api/
         registry.addMapping("/**") // Permitir CORS para todas las rutas que comienzan con /api/
                 .allowedOriginPatterns("*") // Permitir solicitudes desde cualquier origen
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTION") // Métodos permitidos
                 .allowedHeaders("*") // Permitir todos los encabezados
                 .allowCredentials(true); // Permitir credenciales (cookies, autorización)
     }
+
+    @Bean
+    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://127.0.0.1:5500/");
+        configuration.addAllowedOrigin("http://172.16.101.161/");
+        configuration.addAllowedMethod("*"); //
+        configuration.addAllowedHeader("*");  // Permitir todos los encabezados
+        configuration.setAllowCredentials(true);  // Permitir credenciales (cookies, headers de autenticación)
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);  // Aplicar configuración a todas las rutas
+        return source;
+    }
+    @Bean
+    public CorsFilter corsFilter() {
+        return new CorsFilter(corsConfigurationSource());
+    }
 }
+
+
